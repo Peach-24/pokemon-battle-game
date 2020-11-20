c58ed268c8f1ca58fc96e5ae13d19c210d34c8d7
Josh Peachey <joshpeachey24@gmail.com>
2020-11-20 09:34:02 +0000

    Initial commit - created pokedex.js and types.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pokemon-battle-game
+
+A Pokémon battle game that makes use of object-oriented programming, classes, and inquirer.js to emulate the beginning of the iconic Pokemon game (Gen 1).
---
+++ pokedex.js
@@ -0,0 +1,26 @@
+const pokedex = {
+  bulbasaur: {
+    hp: 45,
+    att: 49,
+    def: 49,
+    speed: 45,
+    type: "grass",
+    moves: ["tackle", "growl"],
+  },
+  charmander: {
+    hp: 39,
+    att: 52,
+    def: 43,
+    speed: 65,
+    type: "fire",
+    moves: ["scratch", "growl"],
+  },
+  squirtle: {
+    hp: 44,
+    att: 48,
+    def: 65,
+    speed: 43,
+    type: "water",
+    moves: ["tackle", "tail whip"],
+  },
+};
